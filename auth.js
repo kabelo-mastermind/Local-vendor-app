963194bb9ee84813cf51b4508da4d0bc18f539a7
kabelo-mastermind <0604749776kb@gmail.com>
2025-01-18 12:40:13 +0200

    Supabase client from the global window object

--- auth.js
+++ auth.js
@@ -1,13 +1,5 @@
-// Access the Supabase client from the global window object
-let supabase;
-if (window.supabase) {
-  supabase = window.supabase;
-} else {
-  const SUPABASE_URL = "https://lrdrzttoghslrwoprasi.supabase.co"; // Replace with your actual Supabase URL
-  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyZHJ6dHRvZ2hzbHJ3b3ByYXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwNTk3MjAsImV4cCI6MjA1MjYzNTcyMH0.zGGMI8amEFNHWyvOdOjWiB3fMWWVhkt2-hcLl37XPTc"
-  supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-  window.supabase = supabase; // Attach to the global window object
-}
+// Use the Supabase client from the global window object
+const supabase = window.supabase;
 
 // Sign-up form
 const signupForm = document.getElementById('signupModal');
